74efc96ad7609cd4111716a1a4addafc619bc21c
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-10-12 14:33:24 +0700

    membuta function perulangan dan function main untuk memanggil class conditinalStatment

--- app/src/main/java/com/example/projectandroid/ConditionalExpression.kt
+++ app/src/main/java/com/example/projectandroid/ConditionalExpression.kt
@@ -1,5 +1,9 @@
 package com.example.projectandroid
 
+class ConditionalStatement {
+
+}
+
 // Conditional Expression
 // Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
 // Conditional expression menggunakan if-else, when, dan try-catch
@@ -36,5 +40,38 @@ val number = 10
         println("Invalid Number")
     }
 }
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y=1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatement ()
+    Perulangan()
+}
+
+
 
 

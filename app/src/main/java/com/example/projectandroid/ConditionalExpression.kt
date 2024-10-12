7717279dd8bc574a46c1f5807dd52c6c410da016
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-10-12 14:23:54 +0700

    membuat dimana kondisi benar atau salah

---
+++ app/src/main/java/com/example/projectandroid/ConditionalExpression.kt
@@ -0,0 +1,16 @@
+package com.example.projectandroid
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatment() {
+    println("=== Conditional Expression ===")
+
+// If-Else
+val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+
+    }

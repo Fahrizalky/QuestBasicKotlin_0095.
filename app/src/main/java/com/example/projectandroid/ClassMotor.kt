f1da7bbdb7c8217d3e2c0964ea4f6a0d65f46f56
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-10-12 14:10:45 +0700

    masuk ke dalam tahap utama kotlin

--- app/src/main/java/com/example/projectandroid/ClassMotor.kt
+++ app/src/main/java/com/example/projectandroid/ClassMotor.kt
@@ -1,4 +1,10 @@
 package com.example.projectandroid
 
-class ClassMotor {
+class ClassMotor()
+
+class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1,"mary@gmail.com")
+
 }

960b46b4fa168a2701f0d07faa518be41093c93a
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-10-13 19:24:28 +0700

    menambahkan data ke dalam map mutable

--- app/src/main/java/com/example/projectandroid/Collection.kt
+++ app/src/main/java/com/example/projectandroid/Collection.kt
@@ -85,6 +85,10 @@ fun ContohMap() {
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(shape)
 
+// Menambahkan data ke dalam map mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
 
 
 
